c4b0cba38d784e3d4dfe8faaf25fe866652fb0a6
Mwelwa Nkuta <mwelwankuta@gmail.com>
2021-12-12 15:56:15 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:14.5.0
+
+WORKDIR /app
+
+COPY package.json /app
+COPY package-lock.json /app
+
+RUN npm install
+
+RUN npm run build
+
+CMD ['npm', 'start']
---
+++ src/index.js
@@ -0,0 +1,48 @@
+const merlee = require('merlee.js');
+const { default: axios } = require('axios');
+
+const app = new merlee({ port: 3000, views: 'src/views', static: 'public' });
+
+const repos = [];
+app.handler({ method: 'get', path: '/' }, async (req, res) => {
+  const profileUrl = 'https://api.github.com/users/mwelwankuta/repos';
+  const profile = await axios.get(profileUrl);
+
+  profile.data.forEach(repo => {
+    repos.push({
+      name: repo.name,
+      description: repo.description,
+      link: repo.html_url,
+    });
+  });
+  res.render('home', { repos });
+});
+
+app.handler({ method: 'get', path: '/api' }, async (req, res) => {
+  const profileUrl = 'https://api.github.com/users/mwelwankuta/repos';
+  const profile = await axios.get(profileUrl);
+
+  profile.data.forEach(repo => {
+    repos.push({
+      name: repo.name,
+      description: repo.description,
+      link: repo.html_url,
+    });
+  });
+
+  res.send({ repos }, profile.status);
+});
+
+app.handler({ method: 'post', path: '/search' }, (req, res) => {
+  const searchWord = req.body.search.toLowerCase();
+  const repo = repos.filter(
+    repo =>
+      (repo.name && repo.name.includes(searchWord)) ||
+      (repo.description && repo.description.includes(searchWord))
+  );
+
+  res.render('home', { repos: repo });
+});
+
+app.set('port', 3000);
+app.listen(port => console.log(`listening on port ${port}`));
