3fc2769fd22e7fec721a651f2791e55653e45d5d
Mwelwa Nkuta <mwelwankuta@gmail.com>
2024-04-14 09:03:08 +0200

    chore: updated code to match merlee.js package update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-const merlee = require('merlee.js');
-const getRepos = require('./getRepos');
+const merlee = require("merlee.js");
+const getRepos = require("./getRepos");
 
 const port = process.env.PORT || 3000;
-const app = new merlee();
-
-// app options
-app.set('port', port);
-app.set('views', 'src/views');
-app.set('static', 'public');
-
-// app routes
-app.handler({ method: 'get', path: '/' }, async (req, res) => {
-  const { repos, status } = await getRepos();
-  res.render('home', { repos, search: '' }, status);
+const app = merlee({
+  port,
+  static: "public",
 });
 
-app.handler({ method: 'get', path: '/api' }, async (req, res) => {
+// app routes
+app.handler({ method: "get", path: "/" }, async (req, res) => {
+  const { repos, status } = await getRepos();
+  res.render("home", { repos, search: "" }, status);
+});
+
+app.handler({ method: "get", path: "/api" }, async (req, res) => {
   const { repos, status } = await getRepos();
 
   res.send({ repos }, status);
 });
 
-app.handler({ method: 'post', path: '/search' }, async (req, res) => {
+app.handler({ method: "get", path: "/app" }, async (req, res) => {
+  res.send({ name: "mwelwa" });
+});
+
+app.handler({ method: "post", path: "/search" }, async (req, res) => {
   const { repos, status } = await getRepos();
 
   /**
@@ -30,12 +32,12 @@ app.handler({ method: 'post', path: '/search' }, async (req, res) => {
   const searchWord = req.body.search.toLowerCase();
 
   const repo = repos.filter(
-    repo =>
+    (repo) =>
       (repo.name && repo.name.includes(searchWord)) ||
       (repo.description && repo.description.includes(searchWord))
   );
 
-  res.render('home', { repos: repo, search: searchWord }, status);
+  res.render("home", { repos: repo, search: searchWord }, status);
 });
 
-app.listen(port => console.log(`listening on port ${port}`));
+app.listen((port) => console.log(`listening on port ${port}`));
