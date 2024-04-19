38b22765266a1d33a60f2af8a46b5e963c584b50
Mwelwa Nkuta <mwelwankuta@gmail.com>
2024-04-19 08:33:10 +0200

    feat: merlee.js@0.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import merlee, { NextFunction, Request, Response } from "merlee.js";
+import merlee, { Request, Response } from "merlee.js";
 import getRepos from "./api";
-import cors from "cors";
 
 const port = process.env.PORT;
 const app = merlee({
   port: port ? parseInt(port) : 3000,
   static: "public",
-  middleware: [
-    cors({
-      origin: "*",
-    }),
-  ],
 });
 
 app.handler(
@@ -25,27 +19,15 @@ app.handler(
   {
     method: "get",
     path: "/middle/api",
-    middleware: [
-      (req: Request, _res: Response, _next: NextFunction) => {
-        console.log(req.url);
-        console.log("wont list name");
-        // next(new Error("not found"));
-      },
-      (req: Request, _res: Response, _next: NextFunction) => {
-        console.log(req.url);
-        console.log("will list name");
-        // next(new Error("not found"));
-      },
-    ],
   },
-  async (_req: Request, res: Response) => {
+  async (_req: Request<any>, res: Response) => {
     res.send({ name: "mwelwa" }, 200);
   }
 );
 
 app.handler(
   { method: "get", path: "/api" },
-  async (_req: Request, res: Response) => {
+  async (_req: Request<any>, res: Response) => {
     const { repos, status } = await getRepos();
 
     res.send({ repos }, status);
@@ -54,14 +36,14 @@ app.handler(
 
 app.handler(
   { method: "get", path: "/app" },
-  async (_req: Request, res: Response) => {
+  async (_req: Request<any>, res: Response) => {
     res.send({ name: "mwelwa" });
   }
 );
 
 app.handler(
   { method: "post", path: "/search" },
-  async (req: Request, res: Response) => {
+  async (req: Request<any>, res: Response) => {
     const { repos, status } = await getRepos();
 
     const searchWord = req.body?.search.toLowerCase();
